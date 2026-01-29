4ea3cdb7253186dad2221791e29cce66dd65c202
DivyaAhire11 <divyaahire121@gmail.com>
2026-01-29 20:43:40 +0530

    Linked file allocation

--- 2.1_LinkedAllocation.c
+++ 2.1_LinkedAllocation.c
@@ -69,4 +69,62 @@ void createFile(){
         printf("Not enough free blocks available\n");
         return;
     }
+
+    //Allocate blocks
+    for(i=0,j=0; i<n && i<n && j<block ;i++){
+        if(bitVector[i] == 0){
+            bitVector[i] = 1;
+
+            if(first == -1)
+              first = i;
+            else
+              disk[prev] = i;
+
+            prev = i;
+            j++;
+        }
+    }
+      disk[prev] = -1;  //END of File
+
+      //store in directory
+      strcpy(dir[dirCount].fname,name);
+      dir[dirCount].start = first;
+      dir[dirCount].size = block;
+      dirCount++;
+
+      printf("File created successfully! \n");
+}
+
+void showDirectory(){
+    int i,temp;
+    if(dirCount == 0){
+        printf("\n Directory is empty");
+        return;
+    }
+    
+}
+
+int main() {
+    int choice;
+
+    while(1) {
+        printf("\n--- MENU ---");
+        printf("\n1. Show Bit Vector");
+        printf("\n2. Create New File");
+        printf("\n3. Show Directory");
+        printf("\n4. Exit");
+        printf("\nEnter your choice: ");
+        scanf("%d", &choice);
+
+        switch(choice) {
+            case 1: showBitVector();
+                    break;
+            case 2: createFile();
+                    break;
+            case 3: showDirectory();
+                    break;
+            case 4: return 0;
+            default: printf("Invalid choice!\n");
+        }
+    }
 }
